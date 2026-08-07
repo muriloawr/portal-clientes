6cd6c4fffd2f1971a302f8f6649375a66a3aebdf
muriloawr <murilo@vanzaklabs.com>
2026-08-06 22:39:19 -0300

    sync: atualiza serviços de A Confiteria em CLIENTS

--- functions/clickup-webhook.js
+++ functions/clickup-webhook.js
@@ -26,7 +26,7 @@ const CLIENTS = [
   { name: 'Humara', taskId: 'wdpu2ybtwm', filePath: 'humara/index.html' },
   { name: 'Uplift Fitness', taskId: '86aewgr7t', filePath: 'uplift-fitness/index.html' },
   { name: 'InfinitAge', taskId: '86aeu720q', filePath: 'infinitage/index.html' },
-  { name: 'A Confiteria', taskId: 'wdpu2ydp1t', filePath: 'a-confiteria/index.html' },
+    { name: 'A Confiteria', services: [{ key: 'crm', label: 'CRM', taskId: 'wdpu2ydp1t' }, { key: 'planejamento', label: 'Planejamento', taskId: 'wdpu2yezry' }, { key: 'social-media', label: 'Social Media', taskId: 'wdpu2yf75f' }], filePath: 'a-confiteria/index.html' },,
     { name: 'Orgânico Natural', services: [{ key: 'crm', label: 'CRM', taskId: 'wdpu2ydp1u' }, { key: 'planejamento', label: 'Planejamento', taskId: 'wdpu2yezrr' }, { key: 'social-media', label: 'Social Media', taskId: 'wdpu2yf75c' }], filePath: 'organico-natural/index.html' },,
   {
     name: 'Adah Beauty Tech',
