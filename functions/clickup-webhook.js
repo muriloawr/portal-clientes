9e70cda5c548b4598a20642e14212de9fdc9b399
muriloawr <murilo@vanzaklabs.com>
2026-08-25 13:50:02 -0300

    sync: cadastra cliente Detoni Beauty em CLIENTS

--- functions/clickup-webhook.js
+++ functions/clickup-webhook.js
@@ -60,6 +60,7 @@ const CLIENTS = [
   { name: 'White Align', services: [{ key: 'planejamento', label: 'Planejamento', taskId: 'wdpu2yezrn' }, { key: 'social-media', label: 'Social Media', taskId: 'wdpu2yf75b' }], filePath: 'white-align/index.html' },
   { name: 'CONTREI', projectTaskId: 'wdpu2yft88', filePath: 'contrei/index.html' },
   { name: 'Abrakadabra', projectTaskId: '86aeuct2a', filePath: 'abrakadabra/index.html' },
+  { name: 'Detoni Beauty', projectTaskId: 'wdpu2ygr9z', filePath: 'detoni-beauty/index.html' },
 ];
 
 // Nome do cliente (exatamente como está em CLIENTS[].name) → slug da URL,
